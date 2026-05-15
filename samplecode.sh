a232b963bb11923b4a1b8dfd22b3284d2a52dca5
kishorevelpucharla <kishorevelpucharla@gmail.com>
2026-05-15 12:00:09 +0530

    Create samplecode.sh

---
+++ samplecode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A simple script to print a greeting
+echo "Hello, welcome to your first shell script in demo-project!"
+#!/bin/bash
+echo "Current User: $USER"
+echo "Current Date: $(date)"
+echo "Current Directory: $(pwd)"
